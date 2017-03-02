e50341cf0830937f03c98e4c23de1d78f19189cd
Kellie Higa <kkhiga@ucsd.edu>
2017-03-02 12:54:22 -0800

    thirdpartyAPI lab

---
+++ public/js/facebook.js
@@ -0,0 +1,30 @@
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
+
+function statusChangeCallback(response) {
+  console.log('Facebook login status changed.');
+  console.log(response);
+  // The response object is returned with a status field that lets the
+  // app know the current login status of the person.
+  // Full docs on the response object can be found in the documentation
+  // for FB.getLoginStatus().
+  if (response.status === 'connected') {
+    // Logged into your app and Facebook.
+        console.log('Successfully logged in with Facebook');
+         FB.api('/me?fields=name,first_name,picture.width(480)', changeUser);
+  }
+}
+
+function changeUser(response){
+  console.log('changeUser Called.');
+  console.log(response);
+  // Hide the .facebookLogin <p> element
+  $(".facebookLogin").hide();
+  // // Change name 'Michael Bernstein' to your FB Name
+  $("#name").text(response.name);
+  // // Replace profile picture with fb profile
+  $("#photo").attr('src',response.picture.data.url);
+}
